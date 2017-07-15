4d8aec33024772936beb8d3e6b6f72ad97bad4d3
dklei <dklei@DESKTOP-0R8000F>
2017-07-15 15:34:01 -0300

    Commit inicial com resolução do Staircase

---
+++ HackerRank/src/br/com/klein/denis/practice/algorithms/warmup/C7Staircase.java
@@ -0,0 +1,34 @@
+package br.com.klein.denis.practice.algorithms.warmup;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/staircase 
+ * @status Resolvido
+ * 
+ * Explicação: quando o índice da linha somado ao índice da coluna é maior ou igual ao calor máximo possível de índice da coluna, então
+ * você deve imprimir o #.
+ */
+public class C7Staircase {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long matriz[][] = new long[n][n];
+        
+        int colunaMax = n-1;
+        
+        for (long indiceLinha = 0; indiceLinha<n; indiceLinha++) {
+        	for (long indiceColuna=0; indiceColuna<n; indiceColuna++) {
+        		if(indiceLinha + indiceColuna < colunaMax) {
+        			System.out.print(" ");
+        		} else if (indiceLinha + indiceColuna >= colunaMax && indiceColuna < colunaMax){
+        			System.out.print("#");
+        		} else if(indiceColuna == colunaMax) {
+        			System.out.print("#\n");
+        		}
+        	}
+        }
+        
+    }
+}
---
+++ HackerRank/src/br/com/klein/denis/practice/algorithms/warmup/C9BirthdayCakeCandles.java
@@ -0,0 +1,9 @@
+package br.com.klein.denis.practice.algorithms.warmup;
+
+/**
+ * https://www.hackerrank.com/challenges/birthday-cake-candles 
+ * @status Pendente
+ */
+public class C9BirthdayCakeCandles {
+
+}
