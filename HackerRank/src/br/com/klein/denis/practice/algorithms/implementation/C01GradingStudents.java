175dc489692d97a63b8be54d0a62725cdceaeef3
dklein <dklein@magnasistemas.com.br>
2017-10-23 16:07:55 -0200

    C01 Resolvido

--- HackerRank/src/br/com/klein/denis/practice/algorithms/implementation/C01GradingStudents.java
+++ HackerRank/src/br/com/klein/denis/practice/algorithms/implementation/C01GradingStudents.java
@@ -1,52 +1,55 @@
 package br.com.klein.denis.practice.algorithms.implementation;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
- * https://www.hackerrank.com/challenges/grading 
+ * https://www.hackerrank.com/challenges/grading
+ * 
  * @status Pendente
  */
 public class C01GradingStudents {
-	
+
+	static int[] solve(int[] grades) {
+		int[] resposta = new int[grades.length];
+		for (int i = 0; i < grades.length; i++) {
+			if (grades[i] < 38) {
+				resposta[i] = grades[i];
+				continue;
+			}
+
+			if (grades[i] >= 38) {
+				int proximoMultiploCinco = i;
+				for (int j = grades[i]; j <= 100; j++) {
+					if (j % 5 == 0) {
+						proximoMultiploCinco = j;
+						break;
+					}
+				}
+				if (proximoMultiploCinco - grades[i] < 3) {
+					resposta[i] = proximoMultiploCinco;
+				} else {
+					resposta[i] = grades[i];
+				}
+
+			}
+		}
+
+		return resposta;
+	}
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] grades = new int[n];
-        for(int grades_i=0; grades_i < n; grades_i++){
-            grades[grades_i] = in.nextInt();
-        }
-        
-        List<Integer> retorno = new ArrayList<Integer>();
-        OUTTER: for(int grade : grades) {
-        	if(grade == 100) {
-        		retorno.add(grade);
-        		continue OUTTER;
-        		
-        	}
-        	if(!(grade<38)) {
-        		INNER: for(int i=grade; i<100; i++) {
-        			if(!(i%5==0)) {
-        				continue INNER;
-        			}
-        			if(i-grade<=2) {
-        				retorno.add(i);
-        				continue OUTTER;
-        			} else {
-        				retorno.add(grade);
-        				continue OUTTER;
-        			}
-        		}
-        		
-        	} else {
-        		retorno.add(grade);
-        	}
-        }
-        
-		for(Integer nota : retorno) {
-			System.out.println(nota);
+		int n = in.nextInt();
+		int[] grades = new int[n];
+		for (int grades_i = 0; grades_i < n; grades_i++) {
+			grades[grades_i] = in.nextInt();
 		}
+		int[] result = solve(grades);
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+		}
+		System.out.println("");
+
 	}
 
 }
